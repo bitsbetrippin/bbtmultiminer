83a8641c54aae8bd85a34476e15b79b5cec5533a
BitsBeTrippin_Official <bitsbetrippin@gmail.com>
2019-01-13 15:28:12 -0600

    ProgPoW

---
+++ progminer-1.0.2-amd-windows/start.bat
@@ -0,0 +1,3 @@
+progminer -h
+rem progminer -P stratum://0xb5799fc547306a4802bac047d83485396595f225.bitsV@testnet.progpool.pro:8008
+pause
---
+++ progminer-1.0.2-cuda10.0-windows/start.bat
@@ -0,0 +1,3 @@
+
+progminer.exe -P stratum://0xbf99ebe8e2743ece2dce0ce1093a0a4db79897a2.TitanV@gangnam.wattpool.net:8008
+pause
